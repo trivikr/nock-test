7dd59a5284e275e6071358b6748fd2e1eb715dee
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-07-09 17:52:50 -0700

    index.js

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Buffer } from "buffer";
+import { request, IncomingMessage, RequestOptions } from "http";
+
+/**
+ * @internal
+ */
+export function httpRequest(options: RequestOptions): Promise<Buffer> {
+  return new Promise((resolve, reject) => {
+    const req = request({ method: "GET", ...options });
+    req.on("error", (err: Error) => {
+      reject(err);
+    });
+
+    req.on("response", (res: IncomingMessage) => {
+      const chunks: Array<Buffer> = [];
+      res.on("data", (chunk: any) => {
+        chunks.push(chunk as Buffer);
+      });
+      res.on("end", () => {
+        resolve(Buffer.concat(chunks));
+      });
+    });
+  });
+}
